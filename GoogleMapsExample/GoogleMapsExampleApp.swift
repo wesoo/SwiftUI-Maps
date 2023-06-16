7a336a0360a2c5bee146c8ac7a37403082ffa8eb
Francesco <francesco.beccu@gmail.com>
2023-06-16 10:55:34 +0200

    First Commit
    chore: first commit

---
+++ GoogleMapsExample/GoogleMapsExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleMapsExampleApp.swift
+//  GoogleMapsExample
+//
+//  Created by Francesco Beccu on 16/6/23.
+//
+
+import SwiftUI
+
+@main
+struct GoogleMapsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Podfile
@@ -0,0 +1,10 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '15.0'
+use_frameworks!
+
+target 'GoogleMapsExample' do
+  # Pods for GoogleMapsExample
+    pod 'GoogleUtilities' , '7.11.1'
+    pod 'Google-Maps-iOS-Utils', '4.1.0'
+    pod 'GoogleMaps' , '7.4.0'
+end
